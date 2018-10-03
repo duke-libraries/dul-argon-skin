0a93601a3ed1f196a20c34d41d4ab6de8292bbb6
mikedaul <michael.daul@duke.edu>
2018-10-03 14:43:03 -0400

    refactors code: - moves method / logic into controller - includes some error handling for remote resource - positions button in same <dd> as call number - note: relies on empty button partial in trln_argon (https://github.com/trln/trln_argon/tree/results-button-partial)

--- app/controllers/map_location_controller.rb
+++ app/controllers/map_location_controller.rb
@@ -1,8 +1,42 @@
 class MapLocationController < ApplicationController
-
+  # frozen_string_literal: true
   def show
-    render layout:false
+    @location_content = dul_location_service(params[:loc_n],
+                                             params[:loc_b],
+                                             params[:call_no])
+    render layout: false
     params.permit(:loc_n, :loc_b, :title, :call_no)
   end
 
+  private
+
+  def dul_location_service(collection_code, sublibrary, callno)
+    require 'net/http'
+    require 'uri'
+    uri = URI.parse('https://library.duke.edu/locguide/mapinfo')
+    request = Net::HTTP::Post.new(uri)
+    request['X-Requested-With'] = 'XMLHttpRequest'
+    request.set_form_data(
+      'collection_code' => collection_code,
+      'sublibrary' => sublibrary,
+      'callno' => callno
+    )
+    req_options = {
+      use_ssl: uri.scheme == 'https'
+    }
+    begin
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.read_timeout = 30 # Default is 60 seconds
+        http.request(request)
+      end
+      if response['content-type'] == 'application/json'
+        response.body
+      else
+        false
+      end
+    rescue Net::ReadTimeout => e
+      Rails.logger.error { "#{e.message} #{e.backtrace.join("\n")}" }
+      false
+    end
+  end
 end
