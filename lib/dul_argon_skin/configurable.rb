d42e8ff4433d47cf80212f5bb221dd9246e631a2
Sean Aery <sean.aery@duke.edu>
2018-10-08 15:44:14 -0400

    Simplify methods to initialize / access ENV variables (to resemble convention used in Samvera apps).

---
+++ lib/dul_argon_skin/configurable.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module DulArgonSkin
+  module Configurable
+    extend ActiveSupport::Concern
+    included do
+      # Request System base URL
+      mattr_accessor :request_base_url do
+        ENV['REQUEST_BASE_URL'] || 'https://requests.library.duke.edu'
+      end
+    end
+
+    module ClassMethods
+      def configure
+        yield self
+      end
+    end
+  end
+end
---
+++ lib/dul_argon_skin/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DulArgonSkin
+  VERSION = '0.0.13'
+end
