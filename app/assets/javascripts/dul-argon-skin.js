b8c101a697e738adb7fb71c141cfd26f8a1c33fe
mikedaul <michael.daul@duke.edu>
2018-10-19 20:20:01 -0400

    inserts barcode button to left of call-no label, toggles selectable barcode text on click

--- app/assets/javascripts/dul-argon-skin.js
+++ app/assets/javascripts/dul-argon-skin.js
@@ -4,4 +4,20 @@ $(document).ready(function() {
     $(this).toggleClass('menu-active');
     $('#dul-masthead-region-megamenu').slideToggle();
   });
+
+  $(".barcode-toggle").on("click",function(e) {
+
+    var theBarcode = $(this).parents(".item").data("item-barcode");
+
+    if (theBarcode) {
+      $(this).next(".barcode-display").text(theBarcode);
+      $(this).next(".barcode-display").toggle();
+
+    } else {
+      $(this).next(".barcode-display").text("no barcode");
+      $(this).next(".barcode-display").toggle();
+    }
+
+  });
+
 });
