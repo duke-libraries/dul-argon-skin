42847a33545eb349a2731ffcb2eb03726cd9851a
Cory Lown <cory.lown@duke.edu>
2018-10-26 12:21:06 -0400

    Minor fixes

--- config/initializers/dul_argon_skin.rb
+++ config/initializers/dul_argon_skin.rb
@@ -4,5 +4,6 @@ require 'dul_argon_skin'
 DulArgonSkin.configure do |config|
   # Configs unique to local skin, separate from TrlnArgon
   config.request_base_url = ENV['REQUEST_BASE_URL']
+  config.map_location_service_url = ENV['MAP_LOCATION_SERVICE_URL']
   config.dul_home_url = ENV['DUL_HOME_URL']
 end
