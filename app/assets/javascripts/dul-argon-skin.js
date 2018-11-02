436385de8be8eb551791df78b23d88bd2ac3e124
mikedaul <michael.daul@duke.edu>
2018-11-02 10:19:19 -0400

    allows for toggle of barcodes on show view only - needs an upstream (trln) update to get merged before this will work

--- app/assets/javascripts/dul-argon-skin.js
+++ app/assets/javascripts/dul-argon-skin.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {
+
   /* DUL masthead primary navigation menu toggle */
   $('a#full-menu-toggle').on('click',function(e) {
     $(this).toggleClass('menu-active');
     $('#dul-masthead-region-megamenu').slideToggle();
   });
 
-  $(".barcode-toggle").on("click",function(e) {
 
+  /* display barcodes */
+  $('.barcode-content').each(function() {
     var theBarcode = $(this).parents(".item").data("item-barcode");
-
     if (theBarcode) {
-      $(this).next(".barcode-display").text(theBarcode);
-      $(this).next(".barcode-display").toggle();
-
+      $(this).text(theBarcode);
     } else {
-      $(this).next(".barcode-display").text("no barcode");
-      $(this).next(".barcode-display").toggle();
-    }
-
-  });
-
-  $(document).bind('keypress', function(event) {
-    if( event.which === 66 && event.shiftKey ) {
-        $('.barcode-wrapper').toggle();
+      $(this).text("no barcode");
     }
   });
+
+  $(".barcode-toggle").on("click",function(e) {
+    e.preventDefault();
+    $('.barcode-wrapper').toggle();
+  });
+
 
   /* enable tooltips*/
   $(function () {
