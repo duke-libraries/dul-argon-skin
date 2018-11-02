ceb9fac6f76fab223ee759005b9c50e2062fa277
Cory Lown <cory.lown@duke.edu>
2018-11-02 15:48:26 -0400

    Add an environment alert to the top of each page if configured.

--- lib/dul_argon_skin/configurable.rb
+++ lib/dul_argon_skin/configurable.rb
@@ -4,15 +4,21 @@ module DulArgonSkin
   module Configurable
     extend ActiveSupport::Concern
     included do
-      # Request System base URL.
-      mattr_accessor :request_base_url do
-        ENV['REQUEST_BASE_URL'] ||= 'https://requests.library.duke.edu'
-      end
-      mattr_accessor :map_location_service_url do
-        ENV['MAP_LOCATION_SERVICE_URL'] ||= 'https://library.duke.edu/locguide/mapinfo'
-      end
       mattr_accessor :dul_home_url do
         ENV['DUL_HOME_URL'] ||= 'https://library.duke.edu'
+      end
+
+      # If present this string will display in a banner alert
+      # at the top of each page to indicate which environment
+      # the application is running in. Banner alert NOT shown if blank.
+      mattr_accessor :environment_alert do
+        ENV['ENVIRONMENT_ALERT'] ||= ''
+      end
+
+      # If 'true', output the data being sent to Google Analytics in
+      # the browser console.
+      mattr_accessor :google_analytics_debug do
+        ENV['GOOGLE_ANALYTICS_DEBUG'] ||= 'false'
       end
 
       # If tracking ID is present, activate Google Analytics tracking
@@ -21,10 +27,13 @@ module DulArgonSkin
         ENV['GOOGLE_ANALYTICS_TRACKING_ID'] ||= ''
       end
 
-      # If 'true', output the data being sent to Google Analytics in
-      # the browser console.
-      mattr_accessor :google_analytics_debug do
-        ENV['GOOGLE_ANALYTICS_DEBUG'] ||= 'false'
+      mattr_accessor :map_location_service_url do
+        ENV['MAP_LOCATION_SERVICE_URL'] ||= 'https://library.duke.edu/locguide/mapinfo'
+      end
+
+      # Request System base URL.
+      mattr_accessor :request_base_url do
+        ENV['REQUEST_BASE_URL'] ||= 'https://requests.library.duke.edu'
       end
     end
     module ClassMethods
