ea5d23d50303921a115deb7eeb2c60a74256eb07
mikedaul <michael.daul@duke.edu>
2018-11-07 14:55:29 -0500

    rubocop

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,7 +4,7 @@ class ErrorsController < ApplicationController
   def not_found
      respond_to do |format|
        format.html { render status: 404 }
-       format.json { render json: { error: "Resource not found" }, status: 404 }
+       format.json { render json: { error: 'Resource not found' }, status: 404 }
      end
    end
 end
