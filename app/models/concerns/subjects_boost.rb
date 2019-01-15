31116b906f5fdb267d09d26642c78eef7e757a3a
Cory Lown <cory.lown@duke.edu>
2019-01-15 12:05:14 -0500

    Also boost subjects for recent records and title matches.

---
+++ app/models/concerns/subjects_boost.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module SubjectsBoost
+  extend ActiveSupport::Concern
+
+  # A hack to deal with the fact that
+  # non-Roman subjects like to come up first
+  # since linked fields often duplicate English
+  # language subjects.
+  def subjects_boost(solr_parameters)
+    return unless includes_subject_search?
+    solr_parameters[:bq] ||= []
+    solr_parameters[:bq] << subjects_english_boost_query
+    solr_parameters[:bq] << subjects_title_boost_query
+    solr_parameters[:bf] ||= []
+    solr_parameters[:bf] << subjects_recent_boost_function
+  end
+
+  private
+
+  def subjects_recent_boost_function
+    "linear(recip(rord(#{TrlnArgon::Fields::DATE_CATALOGED_FACET})"\
+    ',1,1000,1000),11,0)^500'
+  end
+
+  def subjects_english_boost_query
+    "#{TrlnArgon::Fields::LANGUAGE_FACET}:English^1000"
+  end
+
+  def subjects_title_boost_query
+    if blacklight_params.key?(:q) &&
+       blacklight_params[:q].present?
+      "#{TrlnArgon::Fields::TITLE_MAIN_INDEXED}:(#{blacklight_params[:q]})^500"
+    elsif blacklight_params.key?('subject') &&
+          blacklight_params['subject'].present?
+      "#{TrlnArgon::Fields::TITLE_MAIN_INDEXED}:"\
+      "(#{blacklight_params['subject']})^500"
+    end
+  end
+
+  def includes_subject_search?
+    blacklight_params.key?('search_field') &&
+      ((blacklight_params['search_field'] == 'subject' ||
+      blacklight_params['search_field'] == 'genre_headings') ||
+      (blacklight_params['search_field'] == 'advanced' &&
+      blacklight_params.fetch('subject', nil).present?))
+  end
+end
