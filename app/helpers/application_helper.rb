5bef43c1b20d8c73ab0e40c179d56761dca99db1
Cory Lown <cory.lown@duke.edu>
2019-01-28 16:24:28 -0500

    Check whether session contains valid controllers for document requests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,12 +6,19 @@ module ApplicationHelper
   end
 
   # rubocop:disable Style/SafeNavigation
-  def search_session_contains_ajax_request?(current_search_session = nil)
+  def search_session_includes_ajax_request?(current_search_session = nil)
     current_search_session &&
       current_search_session.respond_to?(:query_params) &&
       current_search_session.query_params.fetch('format', nil).present? &&
       %w[json xml].include?(current_search_session.query_params
                                                   .fetch('format', ''))
+  end
+
+  def search_session_includes_catalog_controller?(current_search_session = nil)
+    current_search_session &&
+      current_search_session.respond_to?(:query_params) &&
+      %w[catalog trln].include?(current_search_session.query_params
+                                                      .fetch(:controller, ''))
   end
   # rubocop:enable Style/SafeNavigation
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  describe 'search_session_contains_ajax_request?' do
+  describe 'search_session_includes_ajax_request?' do
     context 'when there is no format specified' do
       let(:current_search) do
         Search.create(query_params: { q: 'May Sarton' })
       end
 
       it 'is NOT identified as an ajax search session' do
-        expect(helper.search_session_contains_ajax_request?(current_search)).to(
+        expect(helper.search_session_includes_ajax_request?(current_search)).to(
           be false
         )
       end
@@ -22,9 +22,39 @@ describe ApplicationHelper do
       end
 
       it 'is identified as an ajax search session' do
-        expect(helper.search_session_contains_ajax_request?(current_search)).to(
+        expect(helper.search_session_includes_ajax_request?(current_search)).to(
           be true
         )
+      end
+    end
+  end
+
+  describe 'search_session_includes_catalog_controller?' do
+    context 'when search session includes catalog controller param' do
+      let(:current_search) do
+        Search.create(query_params: { controller: 'trln' })
+      end
+
+      it 'is identified as a catalog controller session' do
+        expect(
+          helper.search_session_includes_catalog_controller?(
+            current_search
+          )
+        ).to(be true)
+      end
+    end
+
+    context 'when search session includes advanced controller param' do
+      let(:current_search) do
+        Search.create(query_params: { controller: 'advanced' })
+      end
+
+      it 'is NOT identified as a catalog controller session' do
+        expect(
+          helper.search_session_includes_catalog_controller?(
+            current_search
+          )
+        ).to(be false)
       end
     end
   end
