92b5941d18e2e36b2a9d031f96062dd8d827fe4b
mikedaul <michael.daul@duke.edu>
2019-04-02 10:38:07 -0400

    moving override to separate model concern

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -18,23 +18,9 @@ class SolrDocument
   include TrlnArgon::SolrDocument
   include TrlnArgon::ItemDeserializer
   include DulArgonSkin::RequestItem
+  include DulArgonSkin::SmsFieldMapping
 
   def rubenstein_record?
     holdings.key?('SCL')
-  end
-
-  def sms_field_mapping
-    @sms_field_mapping ||= {
-      title: proc do
-               truncate(self[TrlnArgon::Fields::TITLE_MAIN].to_s, length: 50)
-             end,
-      location: proc { holdings_to_text },
-      link_to_record: proc { sms_field_mapping_link }
-    }
-  end
-
-  def sms_field_mapping_link
-    TrlnArgon::Engine.configuration.root_url.chomp('/') +
-      Rails.application.routes.url_helpers.solr_document_path(self)
   end
 end
