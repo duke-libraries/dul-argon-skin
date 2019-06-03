a85ecd986a7687ffaa0f82e10d1d5be087f6d6a7
Cory Lown <cory.lown@duke.edu>
2019-06-03 13:25:55 -0400

    Cache solr requests that fetch facet values.

---
+++ app/controllers/advanced_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class AdvancedController < CatalogController
+  def index
+    return if request.method == :post
+
+    cache_key = "#{params.fetch('controller', '')}/"\
+                "#{params.fetch('action', '')}"
+    @response = Rails.cache.fetch(cache_key.to_s, expires_in: 12.hours) do
+      get_advanced_search_facets
+    end
+  end
+
+  private
+
+  def search_action_url(options = {})
+    url_for(options.merge(controller: 'catalog', action: 'index'))
+  end
+
+  # rubocop:disable AccessorMethodName
+  # Method name is from BL Advanced Search
+  def get_advanced_search_facets
+    # We want to find the facets available for the current search, but:
+    # * IGNORING current query (add in facets_for_advanced_search_form filter)
+    # * IGNORING current advanced search facets
+    #   (remove add_advanced_search_to_solr filter)
+    response, = search_results(params) do |search_builder|
+      search_builder.except(:add_advanced_search_to_solr)
+                    .append(:facets_for_advanced_search_form)
+    end
+
+    response
+  end
+  # rubocop:enable AccessorMethodName
+end
