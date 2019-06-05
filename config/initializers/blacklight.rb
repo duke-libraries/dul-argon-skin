87b4ee7d97a24f9aad245cd7daef5c9fa1787a39
mikedaul <michael.daul@duke.edu>
2019-06-05 12:08:50 -0400

    added Google Fi to text carrier list; updated Verizon gateway

---
+++ config/initializers/blacklight.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+TrlnArgon::Engine.config.sms_mappings = {
+  'Virgin' => 'vmobl.com',
+  'AT&T' => 'txt.att.net',
+  'Verizon' => 'vzwpix.com',
+  'Nextel' => 'messaging.nextel.com',
+  'Sprint' => 'messaging.sprintpcs.com',
+  'T Mobile' => 'tmomail.net',
+  'Alltel' => 'message.alltel.com',
+  'Cricket' => 'mms.mycricket.com',
+  'Google Fi' => 'msg.fi.google.com'
+}
