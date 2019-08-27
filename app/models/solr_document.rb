99b4c6599b641dab6fee3027fc242f7eaf142aef
Cory Lown <cory.lown@duke.edu>
2019-08-27 07:44:06 -0400

    Fix rl method bug.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
+# IMPORTANT: Do not put methods here that are also needed
+#            in the TRLN Scope. Shared SolrDocument methods
+#            should go in:
+#            app/models/concerns/solr_document_behavior.rb.
 class SolrDocument
   include SolrDocumentBehavior
-
-  def rubenstein_record?
-    holdings.key?('SCL')
-  end
 end
